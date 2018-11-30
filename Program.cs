1206e1f993456f057263d6e93361320e5cda1c67
Robert Chanphakeo <robertlives@gmail.com>
2018-11-30 23:44:48 +1100

    Initalise

---
+++ Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace vuevanilla
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.Title = "vuevanilla";
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# SPA Template for ASP.NET Core 2.1 with VueJS (Vanilla)
+
+This is a modified template based upon official Microsoft.AspNetCore.SpaTemplate::*
+
+## This template has the following changes:
+
+- Migrated to .Net Core 2.1
+- Removed Typescript
+- Added Serilog
+
+Other than that it has minor changes to VueJS original files.
+
+## How to install:
+
+Clone or download this repo, and install as follows:
+
+```
+dotnet new --install <path to this template directory>
+```
+You can see it listed `dotnet new -l` with the shortname `vuevanilla` so in order to create new projects using this template just use that shortname.
+
+```
+dotnet new vuevanilla -n myspa -o myspa
+cd myspa
+dotnet restore
+npm install
+dotnet run
+```
+To publish, just as usual: `dotnet publish -c release`
